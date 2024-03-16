ecb482e07ee490b4848df283666863366f2e86bc
saikumar8106 <146409244+saikumar8106@users.noreply.github.com>
2024-03-16 15:50:20 +0530

    Update new.3.py

--- python crt/new.3.py
+++ python crt/new.3.py
@@ -1,9 +1,6 @@
-#calculate the value of seven factorial using formula without recursion using for
-
-
+calculate the value of seven factorial using formula without recursion using for
 
 fact =1
 for i in range(1,8):
     fact = fact*i
-    
 print('factorial is', fact)
