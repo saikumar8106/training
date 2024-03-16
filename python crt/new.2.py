bc6c06f5707729108687eb7ddd0a51ac4ba28176
saikumar8106 <146409244+saikumar8106@users.noreply.github.com>
2024-03-16 15:49:15 +0530

    Update new.2.py

--- python crt/new.2.py
+++ python crt/new.2.py
@@ -1,11 +1,9 @@
-#WAP to check the type of triangle based on conditions
-#where u take the input from the user for three sides and classify it accordingly
-# into scalene isosceless equilateral
-
+WAP to check the type of triangle based on conditions
+where u take the input from the user for three sides and classify it accordingly
+into scalane isosceless equilateral
 s1 = int(input('enter side 1 of triangle'))
 s2 = int(input('enter side 2 of triangle'))
 s3 = int(input('enter side 3 of triangle'))
-
 if s1==s2 and s2==s3:
     print('equilateral triangle')
 elif s1==s2 or s1==s3 or s2==s3:
